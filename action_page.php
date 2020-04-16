e37396c8e42bb685eca92d174e597ea7fd246cf3
abhigya-vatsa <63348393+abhigya-vatsa@users.noreply.github.com>
2020-04-16 16:41:52 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+if(isset($_REQUEST['submit'])){
+$_SESSION['name'] = $_POST['name'];
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['mobile'] = $_POST['mobile'];
+}
+
+?>
